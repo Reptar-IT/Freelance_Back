64da3d85002010450560b7b036890f8f00900569
Reco Daley <recodaley@Recos-MacBook-Pro-2.local>
2023-03-21 16:25:09 -0500

    add ability to delete a job record

--- src/modules/jobs/jobsController.ts
+++ src/modules/jobs/jobsController.ts
@@ -1,12 +1,13 @@
 import { Application, Request, Response } from "express";
 
-import { getById, getAll, create } from "./jobsHelper";
+import { getById, getAll, create, deleteJobById } from "./jobsHelper";
 
 export default {
     register(baseEndPoint: string, app: Application) {
         app.post(`${baseEndPoint}`, createJob);
         app.get(`${baseEndPoint}`, getJobs);
         app.get(`${baseEndPoint}/:id`, getJob);
+        app.delete(`${baseEndPoint}/:id`, deleteJob);
         
         async function createJob(req: Request, res: Response) {
             try {
@@ -38,6 +39,20 @@ export default {
                 const params = req.params as any;
                 const { id } = params;
                 const result: any = await getById(id);
+                const { code } = result;
+    
+                res.status(code).send(result);
+            } catch (exception) {
+                console.log(exception);
+                res.status(500).send({ message: 'Internal Server Error' })
+            }
+        }
+
+        async function deleteJob(req: Request, res: Response) {
+            try {
+                const params = req.params as any;
+                const { id } = params;
+                const result: any = await deleteJobById(id);
                 const { code } = result;
     
                 res.status(code).send(result);
--- src/modules/jobs/jobsHelper.ts
+++ src/modules/jobs/jobsHelper.ts
@@ -3,20 +3,34 @@ import { Job } from "../../../mongodb/schemas/index"
 import { Project } from "../../types/types"
 
 export const create = async (record: Project) => {
-    const job = new Job( record );
+    const job = new Job(record);
     const records = await job.save();
 
     return { code: HTTP_CODES.HTTP_STATUS_OK, records }
 }
 
 export const getById = async (id: string) => {
-    const records: any = await Job.findById(id);
+    const record: any = await Job.findById(id);
 
-    return { code: HTTP_CODES.HTTP_STATUS_OK, records }
+    if (!record) return { code: 400, message: `ID ${id} Does not exist` }
+
+    return { code: HTTP_CODES.HTTP_STATUS_OK, records: record }
 }
 
 export const getAll = async () => {
     const records: any = await Job.find({});
 
     return { code: HTTP_CODES.HTTP_STATUS_OK, records }
+}
+
+export const deleteJobById = async (id: string) => {
+    const record: any = await Job.findById(id);
+
+    if (!record) return { code: 400, message: `ID ${id} Does not exist` };
+
+    await Job.findByIdAndDelete(id);
+
+    return {
+        code: HTTP_CODES.HTTP_STATUS_OK, message: `ID ${id} Deleted Successfully`
+    }
 }
