f318a37f3ed49cb2c35f689179b67780b5e93c06
Reco Daley <recodaley@Recos-MacBook-Pro-2.local>
2023-04-15 17:24:10 -0500

    add update and delete functionality to demos

--- src/modules/demos/demosHelper.ts
+++ src/modules/demos/demosHelper.ts
@@ -1,23 +1,55 @@
 import { constants as HTTP_CODES } from "http2";
 import { Demo } from "../../types/types";
 import { demoFieldSpec } from "./demoSpec";
-import { createRecord, getAllRecords } from "../../crud/crudProvider";
 import { fieldSpecValidation, modelName } from "../../crud/fieldSpecValidation";
+import {
+  createRecord,
+  updateRecord,
+  getAllRecords,
+  getRecordById,
+  deleteRecordById,
+} from "../../crud/crudProvider";
 
 const { demo } = modelName;
 
 export const create = async (payload: Demo, reqType: string) => {
-    const validatedRecord = fieldSpecValidation(demoFieldSpec, payload, reqType);
-  
-    return validatedRecord.errors
-      ? { code: 400, errors: validatedRecord.errors }
-      : {
-          code: HTTP_CODES.HTTP_STATUS_OK,
-          records: await createRecord(demo, validatedRecord),
-        };
-  };
+  const validatedRecord = fieldSpecValidation(demoFieldSpec, payload, reqType);
 
-  export const getAll = async () => ({
-    code: HTTP_CODES.HTTP_STATUS_OK,
-    records: await getAllRecords(demo),
-  });
+  return validatedRecord.errors
+    ? { code: 400, errors: validatedRecord.errors }
+    : {
+        code: HTTP_CODES.HTTP_STATUS_OK,
+        records: await createRecord(demo, validatedRecord),
+      };
+};
+
+export const getAll = async () => ({
+  code: HTTP_CODES.HTTP_STATUS_OK,
+  records: await getAllRecords(demo),
+});
+
+export const updateDemoById = async (
+  id: string,
+  payload: Demo,
+  reqType: string
+) => {
+  const validatedRecord = await fieldSpecValidation(
+    demoFieldSpec,
+    payload,
+    reqType
+  );
+
+  return validatedRecord.errors
+    ? { code: 400, errors: validatedRecord.errors }
+    : {
+        code: HTTP_CODES.HTTP_STATUS_OK,
+        records: await updateRecord(demo, { _id: id }, validatedRecord),
+      };
+};
+
+export const deleteDemoById = async (id: string) => {
+  const record: any = await getRecordById(demo, id);
+  if (record.code) return record;
+
+  return await deleteRecordById(demo, { _id: id });
+};
